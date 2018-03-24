ffdbaa8d02d663cd9f16b76e207cbde736ab052e
Nic Fischer <nic.r.fischer@gmail.com>
2018-03-24 16:53:17 -0700

    Update docs for pub and sub classes

--- colugo/py/publisher.py
+++ colugo/py/publisher.py
@@ -3,15 +3,41 @@ from colugo.py.zsocket import Socket
 
 
 class Publisher(Socket):
+    """Socket that binds as a server and publishes to one or many subscriber sockets.
+
+    A node may have multiple publisher sockets, but each node should only have one publisher
+    per topic (ie, a node should not have a two publishers with the same topic name).
+
+    Address and port data are assigned to the specified topic at bind time within the 
+    colugo.py.Socket class.
+
+    To send a message using the publisher socket after it has been constructed, use the 
+    send() method (inherited from colugo.py.Socket)
+
+    Attributes:
+        loop: Reference to the tornado event loop
+        topic: The topic associated with the socket on the network
+    """
 
     def __init__(self, loop, topic):
+        """Constructor for the publisher class
+
+        Args:
+            loop: Reference to the tornado event loop
+            topic: The topic associated with the socket on the network
+        """
         super(Publisher, self).__init__(loop, zmq.PUB)  # Socket.__init__()
         self.topic = topic
 
     def bind(self):
+        """Just calls the colugo.py.Socket.bind() but has a helpful print
+        """
         (addr, port) = super(Publisher, self).bind()  # Socket.bind()
         self.logger.debug("PUB \"{}\" binding to tcp://{}:{}".format(self.topic, addr, port))
 
     def close(self):
+        """Just calls the colugo.py.Socket.close()
+        """
+        # TODO(pickledgator): We can problably remove this and just use the base class method
         # Socket.unbind() is handled within the close call
         super(Publisher, self).close()  # Socket.close()
--- colugo/py/subscriber.py
+++ colugo/py/subscriber.py
@@ -3,22 +3,60 @@ from colugo.py.zsocket import Socket
 
 
 class Subscriber(Socket):
+    """Socket that connects to one or many publisher sockets.
+
+    A node may have multiple subscriber sockets, and each node may have one or more subscribers
+    per topic.
+
+    Address and port data for connection are resolved via a network discovery mechanism.
+
+    Attributes:
+        loop: Reference to the tornado event loop
+        topic: The topic associated with the socket on the network
+        callback: Handler executed when the socket receives messages from a publisher
+    """
 
     def __init__(self, loop, topic, callback):
+        """Constructor for the subscriber class
+
+        Args:
+            topic: The topic associated with the socket on the network
+            callback: Handler executed when the socket receives messages from a publisher
+        """
         super(Subscriber, self).__init__(loop, zmq.SUB)  # Socket.__init__()
         self.topic = topic
         self.callback = callback
         self.set_filter()
 
     def connect(self, address, port):
+        """Connect to a publisher socket at a specified address and port and setup listening
+        task on event loop
+
+        Args:
+            address: Decimal separated string (eg, 127.0.0.1) where service is bound
+            port: int associated with service port
+        """
         self.logger.debug("SUB \"{}\" connecting to tcp://{}:{}".format(self.topic, address, port))
         super(Subscriber, self).connect(address, port)
         super(Subscriber, self).receive(self.callback)
 
     def set_filter(self, filter_string=""):
+        """Helper function to enable zmq.SUBSCRIBER filters
+
+        In zmq, the filter (string or bytes) can be used to screen multi-part messages such 
+        that only messages with the first element of the array match an associated string. 
+        Currently, we configure the subscriber sockets to receive all messages from any publisher.
+        TODO(pickledgator): Add support for filter->separate callbacks?
+
+        Args:
+            filter_string: Setup socket to only allow multi-part messages whose first element 
+                           match this string
+        """
         # "" is a wildcard to accept all messages
         self.zmq_socket.setsockopt_string(zmq.SUBSCRIBE, filter_string)
 
     def close(self):
-        self.logger.debug("Topic \"{}\" disconnecting".format(self.topic))
+        """Just calls the colugo.py.Socket.close()
+        """
+        self.logger.debug("Subscriber \"{}\" disconnecting".format(self.topic))
         super(Subscriber, self).close()  # Client.close()
